a76775bfa163e68c04488ad0fd6f4efd52e7095c
WisnuDwiSeisno <wseisno37@gmail.com>
2024-10-19 08:21:12 +0700

    Melanjutkan yang sudah dikerjakan di kampus

---
+++ app/src/main/java/com/example/myproject2/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.myproject2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.myproject2.ui.theme.MyProject2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyProject2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BasicCompose()
+                }
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+
+@Composable
+fun BasicCompose(modifier: Modifier = Modifier){
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()) {
+
+        Text(text = "Indonesia",
+            style = TextStyle(
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Red,
+            )
+        )
+        Text(text = "1",
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold)
+
+        Image(painter = painterResource(id = R.drawable.logofifa),
+            contentDescription = null)
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyProject2Theme {
+        BasicCompose()
+    }
+}
